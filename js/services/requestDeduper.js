46002da9442eedb07938fbe22dc7398157ed24df
ebimai <ligorbimaj@icloud.com>
2026-07-27 16:16:55 +0300

    feat: add in-flight request deduplication

--- js/services/requestDeduper.js
+++ js/services/requestDeduper.js
@@ -1,3 +1,5 @@
+const pendingRequests = new Map();
+
 /**
  * Shares one in-flight request per key.
  *
@@ -5,9 +7,25 @@
  * @param {() => Promise<unknown>} loader
  * @returns {Promise<unknown>}
  */
-export function dedupeRequest(key, loader) {}
+export function dedupeRequest(key, loader) {
+  if (pendingRequests.has(key)) {
+    return pendingRequests.get(key);
+  }
+
+  const requestPromise = Promise.resolve().then(loader);
+
+  pendingRequests.set(key, requestPromise);
+
+  requestPromise.finally(() => {
+    pendingRequests.delete(key);
+  });
+
+  return requestPromise;
+}
 
 /**
  * Clears all tracked in-flight requests.
  */
-export function clearPendingRequests() {}
+export function clearPendingRequests() {
+  pendingRequests.clear();
+}
